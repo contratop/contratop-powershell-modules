aabb160dc3d8e8eb16288a61800cff7373126afa
ContratopDev <miandroidtabletfirst@gmail.com>
2021-06-20 01:01:42 +0200

    Create contratop_powertoys.ps1

---
+++ contratop_powertoys.ps1
@@ -0,0 +1,6 @@
+$cttpowertoy_ver = 0.1
+function Ctt-PowerToys-Help {
+  Write-Host "Contratop Powertoys $cttpowertoy_ver Help"
+  Write-Host "Ctt-PowerToys-Help : Show this page"
+  Write-Host "Ctt-PowerToys-Version : Print CTT PowerToys Version"
+  }
