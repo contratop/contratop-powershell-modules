b6fc7cf090bea37a3b745c6afe6e361842bbd8c1
ContratopDev <miandroidtabletfirst@gmail.com>
2021-06-20 03:51:14 +0200

    Add files via upload

--- PowerShell_Essentials.psm1
+++ PowerShell_Essentials.psm1
@@ -1,8 +1,13 @@
-﻿function Visit-Molecule-Web {start https://moleculecentral.github.io/centralweb}
+﻿# Variables
+$MyDesktop = "$home\Desktop"
 
-function Get-Forecast-minus {(curl wttr.in/badajoz?n  -UserAgent "curl" ).Content}
 
-function Get-Forecast {(curl http://wttr.in/badajoz -UserAgent "curl" ).Content}
+# Applets
+function Visit-Molecule-Web {start https://moleculecentral.github.io/centralweb}
+
+function Essentials-Forecast-minus {(curl wttr.in/badajoz?n  -UserAgent "curl" ).Content}
+
+function Essentials-Forecast {(curl http://wttr.in/badajoz -UserAgent "curl" ).Content}
 
 function Edit-Profile {notepad++ $profile}
 
@@ -12,5 +17,13 @@ function Get-IPv4Address {Get-NetIPAddress -AddressFamily IPv4 -InterfaceAlias "
 function Get-FreeGames {$data = Invoke-RestMethod https://www.gamerpower.com/api/giveaways?platform=pc; $data | Select-Object title,worth,open_giveaway,platforms,type | Export-Csv $HOME\Desktop\Lista_Juegos.csv -force -notypeinformation; echo "la lista de juegos gratis de la semana se ha explortado correctamente en el Escritorio"}
 
 
-# Lista de descargadores de paquetes extras
-function Get-ContratopPowerToys {wget https://raw.githubusercontent.com/contratop/contratop-powertoys/main/contratop_powertoys.ps1 -OutFile $home\Documents\WindowsPowerShell\contratop_powertoys.ps1}
+# Add Modules
+
+# Auto Updater
+function Essentials-Update {wget https://raw.githubusercontent.com/contratop/contratop-powershell-modules/main/PowerShell_Essentials.psm1 -OutFile 'C:\Program Files\WindowsPowerShell\Modules\ContratopModules'
+                              Write-Host "El paquete de comandos PowerShell Essentials de ContratopDev se ha actualizado correctamente " -ForegroundColor Green
+                              Write-Host ""
+                              Write-Host "Reinicia la PowerShell para que los cambios se apliquen correctamente" -ForegroundColor Yellow}
+
+# Exportar contenido
+Export-ModuleMember -function * -Variable *
